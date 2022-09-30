a1556dac4cca48172cba67871cba61cc03dea3dd
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-30 03:53:05 +0300

    Remove test file explicit creation

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,14 +6,13 @@ apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 chown -hR "$USER":"$USER" /data
-touch /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>"> /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sed -i '/listen 80 default_server;/a\\tlocation /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
