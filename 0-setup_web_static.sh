7e6371fa3be59a3c435412d233a09ffcfd3868ec
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-30 03:44:50 +0300

    Change test html content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,13 @@ sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 sudo chown -hR "$USER":"$USER" /data
 sudo touch /data/web_static/releases/test/index.html
-echo "This is a test" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo sed -i '/listen 80 default_server;/a\\tlocation /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
