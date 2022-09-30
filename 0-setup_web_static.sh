e0968817fe22e789181f3d065520e11de6e0ad0b
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-30 03:34:38 +0300

    Change sed substituted line

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,5 +9,5 @@ sudo chown -hR "$USER":"$USER" /data
 sudo touch /data/web_static/releases/test/index.html
 echo "This is a test" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo sed -i '/listen [::]:80 default_server;/a\\tlocation /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server;/a\\tlocation /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
