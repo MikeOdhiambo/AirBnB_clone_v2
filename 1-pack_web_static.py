5bc57a10e4d8e7bf8e1baff755d1410650a6e4a4
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-03 02:40:54 +0300

    Pycodestyle corrections

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,12 +4,18 @@ from fabric.operations import local
 
 
 def do_pack():
-    """Generates an archive and stores it in /versions with date-generated format"""
+    """
+    Generates an archive and stores it in /versions with date-generated format
+    """
+
     local("mkdir -p versions/")
 
-    check_cmd = local("tar -czf versions/web_static$(date +%Y%m%d%H%M%S).tgz web_static/")
+    check_cmd = local("tar -czf versions/web_static$(date +%Y%m%d%H%M%S).\
+            tgz web_static/")
     if check_cmd.failed:
         return None
     else:
         return local("ls -t versions/ | head -n1")
+
+
 do_pack()
