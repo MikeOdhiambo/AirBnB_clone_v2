bde00d2ab339169a205d780219ad34f9f868a51f
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-03 02:37:54 +0300

    execute do_pack at the end

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """pack_web_static - Generates a .tgz archive of AirBnBclone"""
+from fabric.operations import local
 
 
 def do_pack():
     """Generates an archive and stores it in /versions with date-generated format"""
     local("mkdir -p versions/")
+
     check_cmd = local("tar -czf versions/web_static$(date +%Y%m%d%H%M%S).tgz web_static/")
     if check_cmd.failed:
         return None
     else:
         return local("ls -t versions/ | head -n1")
+do_pack()
