f421345ca0081d48f2e12c52278de88c31593db0
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-03 00:23:32 +0300

    Move chown to after symlink creation

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,6 @@ apt-get -y update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-chown -hR ubuntu:ubuntu /data
 echo "<html>
   <head>
   </head>
@@ -14,5 +13,6 @@ echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data
 sed -i '/listen 80 default_server;/a\\tlocation /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
