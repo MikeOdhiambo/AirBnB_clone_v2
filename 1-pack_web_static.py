42715930586f13cb12fa8ebad4443696e5b5c2e2
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-03 02:25:44 +0300

    Solution 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""pack_web_static - Generates a .tgz archive of AirBnBclone"""
+
+
+def do_pack():
+    """Generates an archive and stores it in /versions with date-generated format"""
+    local("mkdir -p versions/")
+    check_cmd = local("tar -czf versions/web_static$(date +%Y%m%d%H%M%S).tgz web_static/")
+    if check_cmd.failed:
+        return None
+    else:
+        return local("ls -t versions/ | head -n1")
+
+if __name__ == "__main__":
+    from fabric.api import *
+    do_pack()
